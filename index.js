485b0949fc5e9630ac0841eb34dacd3670c907c0
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-09-21 11:25:47 -0400

    check for match

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const board = Document.getElementById("gameboard");
+const board = document.getElementById("gameboard");
 let deck = [];
 let timer, startTimer;
 let firstCard = null;
@@ -11,7 +11,7 @@ async function fetchPokemon(count = 8) {
   let pokemonsCards = []; // array to hold the pokemon cards
 
   for (let i = 1; i <= count; i++) {
-    let randomID = Math.floor(Math.random() * 898) + 1; // get random pokemon ID Generation 1 up to Generation 8.
+    let randomId = Math.floor(Math.random() * 898) + 1; // get random pokemon ID Generation 1 up to Generation 8.
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomId);
     let data = await response.json();
     let image =
@@ -35,3 +35,25 @@ function shuffleDeck(deck) {
   }
   return deck;
 }
+// Fill the deck with pairs of cards
+function fillTable () {
+    const cells= board.querySelectorAll("td");
+    cells.forEach((cell, i) => {
+        cell.innerHTML = "";
+        cell.className="";
+        const poke= deck[i];
+        cell.dataset.id= poke.id;
+        const pokeImage = document.createElement("img");
+        pokeImage.src = poke.img;
+        pokeImage.alt = poke.name;
+        cell.appendChild(pokeImage);
+
+
+    })
+}
+//Click handler
+board.addEventListener("click", (e) => {
+    const cell=e.target.closest("td");
+    if (!cell || lock || cell.classList.contains("matched") || cell.classList.contains("revealed")) return;
+  flip(cell);
+});
