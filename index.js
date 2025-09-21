6f3da34e5c189c1f08e5cff70d6d8306a2bdbe8a
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-09-21 14:31:47 -0400

    project completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const board = document.getElementById("gameboard");
 let deck = [];
-let timer, startTimer;
+let timer, startTime;
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
@@ -54,6 +54,82 @@ function fillTable () {
 //Click handler
 board.addEventListener("click", (e) => {
     const cell=e.target.closest("td");
-    if (!cell || lock || cell.classList.contains("matched") || cell.classList.contains("revealed")) return;
+    if (!cell || lockBoard || cell.classList.contains("matched") || cell.classList.contains("revealed")) return;
   flip(cell);
+   
+  
 });
+
+// Flip card function
+function flip(cell) {
+    cell.classList.add("revealed");
+    if (!firstCard) {
+        firstCard = cell;
+    } else {
+        secondCard = cell;
+        checkForMatch();
+    }
+}
+// Check for match function
+function checkForMatch() {
+    if (firstCard.dataset.id === secondCard.dataset.id) {
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        matches++;
+        resetTurn();
+        if(matches===8){
+            stopTimer();
+            document.getElementById("status").textContent = `Congratulations! You found all matches in ${document.getElementById("timer").textContent}`;
+
+        }
+    } else {
+        lockBoard = true;
+        setTimeout(() => {
+         
+            firstCard.classList.remove("revealed");
+            secondCard.classList.remove("revealed");
+        
+            resetTurn();
+        }, 1000);
+    }
+}
+// Reset turn function
+function resetTurn() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+// Timer functions
+function startTimer() {
+  startTime = Date.now();
+  timer = setInterval(() => {
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    const min = String(Math.floor(elapsed / 60)).padStart(2, "0");
+    const sec = String(elapsed % 60).padStart(2, "0");
+    document.getElementById("timer").textContent = `${min} mins:${sec} ec`;
+  }, 1000);
+}
+function stopTimer() {
+  clearInterval(timer);
+}
+
+// Start game function and shuffle deck
+async function shuffleCards() {
+  const pokes = await fetchPokemon(8);
+  deck = shuffleDeck([...pokes, ...pokes]); // make pairs
+}
+// Start game function
+async function startGame() {
+    stopTimer();
+    matches = 0;
+    document.getElementById("status").textContent = "";
+    document.getElementById("timer").textContent = "00:00";
+
+    await shuffleCards();
+    fillTable();
+    startTimer();
+}
+document.getElementById("restartBtn").addEventListener("click", startGame);
+
+// Start automatically
+window.onload = startGame;
